e81facdbd9e3eac04bfa878b550b75078c74134a
DERRICK ODHIAMBO <125882760+DODHIAMBOO@users.noreply.github.com>
2025-05-27 00:10:29 +0300

    initial commit

--- src/loadd.py
+++ src/loadd.py
@@ -11,14 +11,12 @@ import joblib
 
 def get_model(model_path):
     try:
-        current_dir = os.path.dirname(__file__)  # Directory of loadd.py
+        current_dir = os.path.dirname(__file__)
         model_file = os.path.join(current_dir, model_path)
         with open(model_file, "rb") as mh:
             rf = joblib.load(mh)
             return rf
     except FileNotFoundError:
-        print(f"Model file not found at: {model_file}")
         return None
-    except Exception as e:
-        print(f"Failed to load the model: {e}")
+    except Exception:
         return None
